4f1f9287138bdfff3bb555ca074a9920008c0cdf
Lee Bohyun <55489991+LeeBohyun@users.noreply.github.com>
2021-12-15 20:00:15 +0900

    Update btr0btr.cc

--- mysql-5.6.26/storage/innobase/btr/btr0btr.cc
+++ mysql-5.6.26/storage/innobase/btr/btr0btr.cc
@@ -5,7 +5,7 @@ Copyright (c) 2012, Facebook Inc.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
-Foundation; version 2 of the License.
+Foundation; version 2 of the License. - Lee bo hyun
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
